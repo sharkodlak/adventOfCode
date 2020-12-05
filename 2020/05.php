973548f452b8ecd979f9b24555e2c85da74874f6
Pavel Štětina <github@shark.kom.cz>
2020-12-05 10:29:32 +0100

    Day 5 first star.

---
+++ 2020/05.php
@@ -0,0 +1,23 @@
+#!/usr/bin/env php
+<?php declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$lines = file(substr(__FILE__, 0, -4) . '/input.txt');
+$boardingPasses = [];
+
+foreach ($lines as $bp => $line) {
+	$boardingPasses[$bp] = 0;
+	for ($i = 0; $i < 10; ++$i) {
+		if (in_array($line[$i], ['B', 'R'])) {
+			$boardingPasses[$bp] |= 1;
+		}
+		if ($i != 9) {
+			$boardingPasses[$bp] = $boardingPasses[$bp] << 1;
+		}
+	}
+}
+
+$max = max($boardingPasses);
+
+echo "Max seat ID: $max .\n";
