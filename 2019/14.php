69110c35a21ea85d97ee96be64b62b7eb04f8b09
Pavel Štětina <github@shark.kom.cz>
2020-12-05 23:18:49 +0100

    Unfinished modification of day 14.

--- 2019/14.php
+++ 2019/14.php
@@ -11,6 +11,7 @@ $file = [
 	'7 A, 1 D => 1 E',
 	'7 A, 1 E => 1 FUEL',
 ];
+/*
 $file = [
 	'9 ORE => 2 A',
 	'8 ORE => 3 B',
@@ -45,7 +46,6 @@ $file = [
 	'1 VJHF, 6 MNCFX => 4 RFSQX',
 	'176 ORE => 6 VJHF',
 ];
-/*
 $file = [
 	'171 ORE => 8 CNZTR',
 	'7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL',
@@ -73,6 +73,9 @@ class Chemical {
 	private $order;
 	private $quantity;
 	private $reactants = [];
+	private $nextChemical;
+	private $completeReactionAmount = 0;
+	private $completeReactionReactantQuantities = [];
 
 	public function __construct(string $name, int $order, int $quantity, array $reactants = []) {
 		$this->name = $name;
@@ -86,6 +89,11 @@ class Chemical {
 
 	public function __toString(): string {
 		return implode(', ', $this->reactants) . " => {$this->quantity} {$this->name}";
+	}
+
+	public function setNextChemical(Chemical $nextChemical): self {
+		$this->nextChemical = $nextChemical;
+		return $this;
 	}
 
 	private function cmpReactants(Reactant $a, Reactant $b): int {
@@ -108,15 +116,87 @@ class Chemical {
 		return $this->reactants;
 	}
 
-	public function findReaction(array $sources): Chemical {
+	public function findReaction(array $sources, int $quantity = 1, array $amounts = []): ChemicalQuantity {
 		foreach ($this->reactants as $reactant) {
 			$sourceQuantity = $sources[$reactant->getName()] ?? 0;
 			$reactantAmount = $reactant->getAmount();
 			if ($reactantAmount > $sourceQuantity) {
-				return $reactant->findReaction($sources);
+				return $reactant->findReaction($sources, $quantity, $amounts);
 			}
 		}
+		return ChemicalQuantity::getInstance($this, $quantity);
+	}
+
+	// 1C = 7A/10 + 1B(1): 7:10
+	// 1D = (7A + 7A)/10 + 1B: 14/10
+	public static function fillCompleteReactionAmount(Chemical $chemical): int {
+		$multiplier = 5;
+		do {
+			foreach ($chemical->reactants as $reactant) {
+				$reactant->addCompleteReactionAmount($multiplier);
+			}
+			$reactant = reset($chemical->reactants);
+			echo $chemical->getName(), "\n";
+			// Vím, že potřebuji $multiplier * $chemical->quantity
+			// Pokud je nastavené $chemical->completeReactionAmount, měl bych najít lcm($multiplier * quantity, completeReactionAmount)
+			// Pomocí lcm / ($multiplier * quantity) násobit všechny předchozí completeReactionAmount
+			echo "Quantity: $chemical->quantity, Multiplier: $multiplier, CRA: ", $chemical->completeReactionAmount, ", ReactantQuantities: ", implode(", ", $chemical->completeReactionReactantQuantities), ".\n";
+			exit;
+			if ($chemical->completeReactionReactantQuantities) {
+				$lcmInput = $chemical->completeReactionReactantQuantities;
+				$lcmInput[] = $multiplier * $chemical->getCompleteReactionAmount();
+				$multiplier = \Calc\Arithmetic\ArithmeticFunctions::lcmm($lcmInput);
+			}
+		} while ($chemical = $chemical->nextChemical);
+		return $multiplier;
+	}
+
+	public function getCompleteReactionAmount(): int {
+		return $this->completeReactionAmount;
+	}
+
+	public function addCompleteReactionAmount(int $amount): int {
+		$this->completeReactionReactantQuantities[] = $amount;
+		$this->completeReactionAmount += $amount;
+		return $this->completeReactionAmount;
+	}
+
+	private function multiplyCompleteReactionAmount(int $multiplier): self {
+		$this->completeReactionAmount *= $multiplier;
 		return $this;
+	}
+}
+
+class ChemicalQuantity {
+	private static $instance;
+	private $chemical;
+	private $quantity;
+
+	public static function getInstance(Chemical $chemical, int $quantity) {
+		if (!isset(self::$instance)) {
+			self::$instance = new self($chemical, $quantity);
+		} else {
+			self::$instance->chemical = $chemical;
+			self::$instance->quantity = $quantity;
+		}
+		return self::$instance;
+	}
+
+	public function __construct(Chemical $chemical, int $quantity) {
+		$this->chemical = $chemical;
+		$this->quantity = $quantity;
+	}
+
+	public function __call(string $methodName, array $args) {
+		return $this->chemical->$methodName(...$args);
+	}
+
+	public function getChemical() {
+		return $this->chemical;
+	}
+
+	public function getQuantity() {
+		return $this->quantity;
 	}
 }
 
@@ -129,109 +209,22 @@ class Reactant {
 		$this->chemical = $chemical;
 	}
 
+	public function __call(string $methodName, array $args) {
+		return $this->chemical->$methodName(...$args);
+	}
+
 	public function __toString(): string {
-		return "-{$this->amount} {$this->chemical->getName()}";
+		return "-{$this->amount} {$this->getName()}";
 	}
 
 	public function getAmount(): int {
 		return $this->amount;
 	}
 
-	public function getName(): string {
-		return $this->chemical->getName();
-	}
-
-	public function getOrder(): int {
-		return $this->chemical->getOrder();
-	}
-
-	public function findReaction(array $sources): Chemical {
-		return $this->chemical->findReaction($sources);
+	public function addCompleteReactionAmount($multiplier = 1): int {
+		return $this->chemical->addCompleteReactionAmount($multiplier * $this->amount);
 	}
 }
-
-/*
-FUEL1
-7   1
-|     E1
-|   7 1
-| /   D1
-|   7 1
-| /   C1
-|   7 1
-A10   B1
-10  1
-ORE
-
-FUEL1
-28  1
-A10 |
-10  /
-ORE
-
-
-
-1 = 1 + 30(28)
-2 = 2 + 60(56)
-3 = 3 + 90(84)
-4 = 4 + 120(112)
-5 = 5 + 140(140)
-
-
-ORE(1)
-   >9= A(2)
-        >1-------------\
-        >3---\         |
-   >8= B(3)  |         |
-        >4---+= AB(1)  |
-        >5---\    >2---|--------\
-   >7= C(5)  |         |        |
-        >7---+= BC(1)  |        |
-                  >3---|--------+
-        >4-------------+=CA(1)  |
-                           >4---+= FUEL(1)
-
--FUEL: 2520
--AB:   5040
--BC:   7560
--CA:  10080
--A:   10080 *3(>AB)
--B:   37800 *4(>AB)
--C:   10080 *5(>CA) *7(>BC)
-ORE:  57960 *2(>B) *3(>A)
-
-ORE(1)
- >157= NZVS(5)
-          >29---------------------------\
-           >7---------------\           |
- >165= DCFZ(6)              |           |
-           >3---------------+           |
-           >7---\           |           |
- >179= PSHF(7)  |           |           |
-           >7---= XJWVT(2)  |           |
-                      >44---|-----------+
-          >10---------------+           |
-                            |           |
-           >8----\          |           |
- >177= HKGWZ(5)  |          |           |
-            >5---|----------= KHKGT(8)  |
-                 |                 >5---+
-           >12---+                      |
-           >48---|----------------------+
- >165= GPVTF(2)  |                      |
-            >1---= QDVJ(9)              |
-                       >1---------------+
-            >9--------------------------= FUEL(1)
-
--FUEL:    1 * 2(GPVTF>QDVJ)
-NZVS:    29
-XJWVT:   44
-KHKGT:    5
-HKGWZ:   48
-QDVJ:     1
-GPVTF:    9
-
-*/
 
 $reactions = [];
 $regexp = '~(?P<quantity>\d+) (?P<chemical>[A-Z]+)~';
@@ -253,6 +246,7 @@ $order = 0;
 $chemicals = [
 	'ORE' => new Chemical('ORE', $order++, 1),
 ];
+$nextChemical = $chemicals['ORE'];
 while ($reactions) {
 	foreach ($reactions as $chemicalName => $reaction) {
 		$reactants = [];
@@ -265,7 +259,10 @@ while ($reactions) {
 			$reactants[$reactantName] = new Reactant($amount, $chemicals[$reactantName]);
 		}
 		if ($reactantsExists) {
-			$chemicals[$chemicalName] = new Chemical($chemicalName, $order++, $reaction['quantity'], $reactants);
+			$chemical = new Chemical($chemicalName, $order++, $reaction['quantity'], $reactants);
+			$chemical->setNextChemical($nextChemical);
+			$chemicals[$chemicalName] = $chemical;
+			$nextChemical = $chemical;
 			unset($reactions[$chemicalName]);
 			break;
 		}
@@ -276,7 +273,7 @@ $sums = [
 	'FUEL' => 1,
 ];
 $i = 0;
-while (array_diff_key($sums, array_flip(['ORE']))) {
+while (array_diff_key($sums, ['ORE' => 1])) {
 	$chemical = null;
 	foreach ($sums as $chemicalName => $amount) {
 		if (empty($chemical) || $chemicals[$chemicalName]->getOrder() > $chemical->getOrder()) {
@@ -296,6 +293,13 @@ while (array_diff_key($sums, array_flip(['ORE']))) {
 echo "Number of ORE to process is: {$sums['ORE']} .\n";
 
 
+$amount = Chemical::fillCompleteReactionAmount($chemicals['C']);
+var_dump($amount);
+exit;
+
+
+
+
 $initialORE = 1000000000000;
 $sources = [
 	'ORE' => $initialORE,
@@ -309,17 +313,17 @@ while (isset($sources['ORE']) && $sources['ORE'] > 0) {
 		$sources['ORE'] = $initialORE % $completeReaction['ORE'];
 		$sources['FUEL'] *= floor($initialORE / $completeReaction['ORE']);
 	}
-	$chemical = $chemicals['FUEL']->findReaction($sources);
-	if ($chemical->getName() == 'ORE') {
+	$chemicalQuantity = $chemicals['FUEL']->findReaction($sources);
+	if ($chemicalQuantity->getName() == 'ORE') {
 		break;
 	}
-	foreach ($chemical->getReactants() as $reactantName => $reactant) {
+	foreach ($chemicalQuantity->getReactants() as $reactantName => $reactant) {
 		$sources[$reactantName] -= $reactant->getAmount();
 		if ($sources[$reactantName] == 0) {
 			unset($sources[$reactantName]);
 		}
 	}
-	$sources[$chemical->getName()] = $chemical->getQuantity() + ($sources[$chemical->getName()] ?? 0);
+	$sources[$chemicalQuantity->getName()] = $chemicalQuantity->getChemical()->getQuantity() + ($sources[$chemicalQuantity->getName()] ?? 0);
 }
 
 echo "Maximum FUEL created is {$sources['FUEL']} .\n";
