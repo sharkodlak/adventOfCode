2a8e313a8079ee31c1f58ad526fe6266f5f281aa
Pavel Štětina <github@shark.kom.cz>
2020-12-10 11:40:14 +0100

    Day 08 refactoring.

--- 2020/08.php
+++ 2020/08.php
@@ -4,6 +4,12 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $instructions = file(substr(__FILE__, 0, -4) . '/example.txt');
+foreach ($instructions as &$instruction) {
+	[$opcode, $arg] = explode(' ', $instruction);
+	$arg = (int) $arg;
+	$instruction = [$opcode, $arg];
+}
+unset($instruction);
 $instructionSet = [
 	'acc' => fn($pointer, $acc, $arg) => [++$pointer, $acc + $arg],
 	'jmp' => fn($pointer, $acc, $arg) => [$pointer + $arg, $acc],
@@ -12,14 +18,13 @@ $instructionSet = [
 
 class Computer {
 	private $instructionSet = [];
+	private $acc;
+	private $pointer;
+	private $visited;
 
 	public function __construct(array $instructionSet) {
 		$this->instructionSet = $instructionSet;
 	}
-
-	private $acc;
-	private $pointer;
-	private $visited;
 
 	public function execute(array $instructions) {
 		$this->pointer = 0;
@@ -27,8 +32,7 @@ class Computer {
 		$this->visited = [];
 		while ($this->pointer < count($instructions) && !isset($this->visited[$this->pointer])) {
 			$this->visited[$this->pointer] = 1;
-			[$opcode, $arg] = explode(' ', $instructions[$this->pointer]);
-			$arg = (int) $arg;
+			[$opcode, $arg] = $instructions[$this->pointer];
 			[$this->pointer, $this->acc] = $this->instructionSet[$opcode]($this->pointer, $this->acc, $arg);
 		}
 		return $this->acc;
