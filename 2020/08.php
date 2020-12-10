c65722dd2ce39240a275540e2f76324bd80e13d3
Pavel Štětina <github@shark.kom.cz>
2020-12-10 11:34:07 +0100

    Day 08 refactoring.

--- 2020/08.php
+++ 2020/08.php
@@ -3,22 +3,38 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$instructions = file(substr(__FILE__, 0, -4) . '/input.txt');
-$pointer = 0;
-$acc = 0;
-$visited = [];
-
+$instructions = file(substr(__FILE__, 0, -4) . '/example.txt');
 $instructionSet = [
 	'acc' => fn($pointer, $acc, $arg) => [++$pointer, $acc + $arg],
 	'jmp' => fn($pointer, $acc, $arg) => [$pointer + $arg, $acc],
 	'nop' => fn($pointer, $acc) => [++$pointer, $acc],
 ];
 
-while (!in_array($pointer, $visited)) {
-	$visited[$pointer] = $pointer;
-	[$opcode, $arg] = explode(' ', $instructions[$pointer]);
-	$arg = (int) $arg;
-	[$pointer, $acc] = $instructionSet[$opcode]($pointer, $acc, $arg);
+class Computer {
+	private $instructionSet = [];
+
+	public function __construct(array $instructionSet) {
+		$this->instructionSet = $instructionSet;
+	}
+
+	private $acc;
+	private $pointer;
+	private $visited;
+
+	public function execute(array $instructions) {
+		$this->pointer = 0;
+		$this->acc = 0;
+		$this->visited = [];
+		while ($this->pointer < count($instructions) && !isset($this->visited[$this->pointer])) {
+			$this->visited[$this->pointer] = 1;
+			[$opcode, $arg] = explode(' ', $instructions[$this->pointer]);
+			$arg = (int) $arg;
+			[$this->pointer, $this->acc] = $this->instructionSet[$opcode]($this->pointer, $this->acc, $arg);
+		}
+		return $this->acc;
+	}
 }
 
+$computer = new Computer($instructionSet);
+$acc = $computer->execute($instructions);
 echo "Accumulator value $acc .\n";
