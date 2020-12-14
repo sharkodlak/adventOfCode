eb11e26a32e05a64c2eff76c021d18cde895d097
Pavel Štětina <github@shark.kom.cz>
2020-12-14 18:46:01 +0100

    Day 13 second star.

--- 2020/13.php
+++ 2020/13.php
@@ -6,8 +6,13 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $lines = file(substr(__FILE__, 0, -4) . '/input.txt');
 
 $timestamp = (int) $lines[0];
-preg_match_all('~(?<=^|,)\d+~', $lines[1], $matches);
-$busLines = $matches[0];
+preg_match_all('~(?<=^|,)\d+|x~', $lines[1], $matches);
+$busLines = [];
+foreach ($matches[0] as $i => $bus) {
+	if ($bus != 'x') {
+		$busLines[$i] = (int) $bus;
+	}
+}
 $departs = [];
 
 foreach ($busLines as $bus) {
@@ -20,3 +25,31 @@ $tillNextDepart = min($departs);
 $bus = array_search($tillNextDepart, $departs);
 
 printf("Bus %d departs in %d. Product is %d .\n", $bus, $tillNextDepart, $bus * $tillNextDepart);
+
+$busStep = 0;
+$step = $busLines[$busStep];
+$offset = array_search($busStep, $busLines);
+$t = 0;
+
+do {
+	$t += $step;
+	$found = true;
+	//echo "Time $t\n";
+	foreach ($busLines as $i => $bus) {
+		$sinceLastDepart = ($t + $i) % $bus;
+		//echo "$i, $bus: $sinceLastDepart\n";
+		if ($sinceLastDepart == 0) {
+			if ($i > $busStep) {
+				$prev = $step;
+				$step = \Calc\Arithmetic\ArithmeticFunctions::lcm($step, $bus);
+				//echo "New step: lcm($prev, $bus) = $step.\n";
+				$busStep = $i;
+			}
+		} else {
+			$found = false;
+			break;
+		}
+	}
+} while(!$found);
+
+printf("First time with continuous departs is %d .\n", $t);
