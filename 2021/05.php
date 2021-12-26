dd3246b23211ca0401ec5c54144280f477632501
Pavel Stetina <pavel.stetina@devcity.tech>
2021-12-26 18:17:42 +0100

    Day 05 second star.

--- 2021/05.php
+++ 2021/05.php
@@ -14,12 +14,14 @@ $sum = 0;
 
 foreach ($lines as $line) {
 	preg_match('~^(\d+),(\d+) -> (\d+),(\d+)$~', $line, $matches);
+	$fnX = $matches[1] < $matches[3] ? fn($x) => ++$x : ($matches[1] > $matches[3] ? fn($x) => --$x : fn($x) => $x);
+	$fnY = $matches[2] < $matches[4] ? fn($y) => ++$y : ($matches[2] > $matches[4] ? fn($y) => --$y : fn($y) => $y);
 
-	if ($matches[1] === $matches[3] || $matches[2] === $matches[4]) {
-		foreach (range($matches[1], $matches[3]) as $x) {
-			foreach (range($matches[2], $matches[4]) as $y) {
-				$map[$x][$y] = 1 + ($map[$x][$y] ?? 0);
-			}
+	for ($x = $matches[1], $y = $matches[2]; true; $x = $fnX($x), $y = $fnY($y)) {
+		echo "$x,$y\n";
+		$map[$x][$y] = 1 + ($map[$x][$y] ?? 0);
+		if ($x == $matches[3] && $y == $matches[4]) {
+			break;
 		}
 	}
 }
