e614190e977a292f3166c1b03ec6f772825fb276
Pavel Stetina <pavel.stetina@devcity.tech>
2021-12-26 12:45:27 +0100

    Day 4 first star.

--- 2021/04.php
+++ 2021/04.php
@@ -3,25 +3,79 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$lines = file(__DIR__ . '/inputs/04.sample.txt');
-$numbersDrawn = explode(',', array_shift($lines));
+$inputFile = sprintf('%s/inputs/%02d%s.txt',
+	__DIR__,
+	basename(__FILE__, '.php'),
+	$argc == 1 ? '.sample' : ''
+);
+$lines = file($inputFile);
+$drawnNumbers = explode(',', array_shift($lines));
+$drawnNumbers = array_map('intval', $drawnNumbers);
 
 class Board {
-	private array $rows = [];
+	private array $numbers = [];
+	private array $cols = [0, 0, 0, 0, 0];
+	private array $rows = [0, 0, 0, 0, 0];
+	private bool $won = false;
 
-	public function setRow(array $numbers): self {
-		$this->rows[] = $numbers;
+	public function __construct(private int $boardNumber) {}
+
+	public function addNumber(int $number): self {
+		$this->numbers[] = $number;
 		return $this;
+	}
+
+	public function drawn(int $number): self {
+		$position = \array_search($number, $this->numbers, true);
+		if ($position !== false) {
+			unset($this->numbers[$position]);
+			$row = \floor($position / 5);
+			$col = $position % 5;
+			//echo "b{$this->boardNumber}: $position = [$col, $row]\n";
+
+			if (++$this->cols[$col] === 5 || ++$this->rows[$row] === 5) {
+				$this->won = true;
+			}
+		}
+
+		return $this;
+	}
+	
+	public function won(): bool {
+		return $this->won;
+	}
+	
+	public function getScore(): int {
+		return array_sum($this->numbers);
 	}
 }
 
+$boards = [];
 $lastBoard = null;
 
 foreach ($lines as $line) {
-	if ($lines === '\n') {
-		exit('asdfghj');
+	if (trim($line) === '') {
+		$lastBoard = new Board(count($boards));
+		$boards[] = $lastBoard;
+	} else {
+		for ($i = 0; $i < 5; ++$i) {
+			$number = intval(substr($line, $i * 3, 2));
+			$lastBoard->addNumber($number);
+		}
 	}
 }
 
-printf("Bingo final score is %d . It's score is %d multiplied by %d lest number.\n", $score * $number, $score, $number);
+foreach ($drawnNumbers as $drawn) {
+	//echo "number drawn: $drawn\n";
+	foreach ($boards as $board) {
+		$board->drawn($drawn);
+
+		if ($board->won()) {
+			$score = $board->getScore();
+			break(2);
+		}
+	}
+}
+
+printf("Bingo final score is %d . Score is %d multiplied by %d last number.\n", $score * $drawn, $score, $drawn);
 
