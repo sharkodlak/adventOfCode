cd57c396199d325237209fa99c4718e6d6ed391c
Pavel Stetina <pavel.stetina@devcity.tech>
2021-12-26 11:21:06 +0100

    Rewrite in progress

--- 2020/18.php
+++ 2020/18.php
@@ -3,25 +3,58 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$lines = file(substr(__FILE__, 0, -4) . '/input.txt');
 $lines = [
-	'1 + 2 * 3 + 4 * 5 + 6',
-	'1 + (2 * 3) + (4 * (5 + 6))',
+	"4" => [4],
+	"( 3 )" => [[3]],
+	"1 + 2 * 3 + 4 * 5 + 6\n" => [[[[[1, '+', 2], '*', 3], '+', 4], '*', 5], '+', 6],
+		/*
+		//[[[1 + 2] * [3 + 4]] * [5 + 6]],
+	"1 + (2 * 3) + (4 * (5 + 6))\n" =>
+		[[1 + [2 * 3]] + [4 * [5 + 6]]],
+	'2 * 3 + (4 * 5)' =>
+		[[2 * 3] + [4 * 5]],
+		//[2 * [3 + [4 * 5]]],
+	'5 + (8 * 3 + 9 + 3 * 4 * 3)' =>
+		[5 + [[[[[8 * 3] + 9] + 3] * 4] * 3]],
+		//[5 + [[[8 * [[3 + 9] + 3]] * 4] * 3]],
+	'5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))' =>
+		[[5 * 9] * [[[[[7 * 3] * 3] + 9] * 3] + [[8 + 6] * 4]]],
+		//[[5 * 9] * [[[7 * 3] * [3 + 9]] * [3 + [[8 + 6] * 4]]]],
+	'((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2' =>
+		[[[[[[[2 + 4] * 9] * [[[6 + 9] * 8] + 6]] + 6] + 2] + 4] * 2],
+		//[[[[[[2 + 4] * 9] * [[[6 + 9] * [8 + 6]] + 6]] + 2] + 4] * 2],
+	*/
 ];
+//$lines = file(substr(__FILE__, 0, -4) . '/input.txt');
 $sum = 0;
 
 class Calc {
 	private static $ops;
+	private $operatorPrecedence;
 
-	public function __construct() {
+	public function __construct(array $operatorPrecedence) {
 		self::$ops = [
-			'+' => fn($a, $b) => $a + $b,
-			'*' => fn($a, $b) => $a * $b,
+			'+' => fn(int $a, int $b) => $a + $b,
+			'*' => fn(int $a, int $b) => $a * $b,
 		];
+		$this->operatorPrecedence = $operatorPrecedence;
 	}
 
-	public function eval($a, $op, $b) {
+	public function calculate($a, $op, $b): int {
+		//echo "$a $op $b\n";
 		return self::$ops[$op]($a, $b);
+	}
+
+	public function eval(array $tokens): int {
+		$a = $tokens[0];
+		$a = is_array($a) ? $this->eval($a) : (int) $a;
+		for ($i = 1; $i < count($tokens); ++$i) {
+			$op = $tokens[$i];
+			$b = $tokens[++$i];
+			$b = is_array($b) ? $this->eval($b) : (int) $b;
+			$a = $this->calculate($a, $op, $b);
+		}
+		return $a;
 	}
 
 	public function tokenize(string $line) {
@@ -34,25 +67,28 @@ class Calc {
 		$current = &$tokens[];
 		for (; $i < strlen($line); ++$i) {
 			$c = $line[$i];
-			if ($c == ' ') {
+			if (in_array($c, ['(', ')'])) {
+				$current = $c;
 				$current = &$tokens[];
-			} else if ($c == '(') {
-				++$i;
-				$current = self::subtokenize($line, $i);
-			} else if ($c == ')') {
-				break;
-			} else {
-				$current .= $line[$i];
+			} else if ($c == ' ') {
+				if (isset($current)) {
+					$current = &$tokens[];
+				}
+			} else if ($c != PHP_EOL) {
+				$current .= $c;
 			}
 		}
-		return $tokens;
+		return array_filter($tokens);
 	}
 }
 
-$calc = new Calc;
+$operatorPrecedence = ['(' => 2, ')' => 2, '+' => 0, '*' => 0];
+$calc = new Calc($operatorPrecedence);
 
-foreach ($lines as $line) {
-	var_dump($calc->tokenize($line));
+foreach ($lines as $line => $formula) {
+	$tokens = $calc->tokenize($line);
+	var_dump($tokens);
+	//$sum += $calc->eval($tokens);
 }
 
 printf("Sum of resulting values: %d .\n", $sum);
