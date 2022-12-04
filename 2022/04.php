6aba6b44cdbc1981fb70ffedc461598785afe77b
Pavel Stetina <pavel.stetina@mallgroup.com>
2022-12-04 11:29:35 +0100

    Advent of code 2022 day 04, second star.

--- 2022/04.php
+++ 2022/04.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $inputLoader = new \adventOfCode\lib\InputLoader(__FILE__, in_array('--sample', $argv));
 $assignmentsFullyContained = 0;
-
+$assignmentsOverlaps = 0;
 
 foreach ($inputLoader as $line) {
     [$elfA, $elfB] = explode(',', $line);
@@ -15,7 +15,12 @@ foreach ($inputLoader as $line) {
     if ($fullyContains) {
         ++$assignmentsFullyContained;
     }
-    //\adventOfCode\lib\Dumper::dump([$a, $A, $b, $B, $fullyContains]);
+    $overlaps = $b <= $a && $a <= $B || $b <= $A && $A <= $B || $a <= $b && $b <= $A || $a <= $B && $B <= $A;
+    if ($overlaps) {
+        ++$assignmentsOverlaps;
+    }
+    //\adventOfCode\lib\Dumper::dump(['a' => $a, 'A' => $A, 'b' => $b, 'B' => $B, 'b <= a <= B' => $b <= $a && $a <= $B, $b <= $A && $A <= $B, $a <= $b && $b <= $A, $a <= $B && $B <= $A]);
 }
 
 echo "Assignments fully contained: $assignmentsFullyContained .\n";
+echo "Assignments overlaps: $assignmentsOverlaps .\n";
