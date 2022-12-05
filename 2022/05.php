dc64a2c43c4e42242a7ce971de1394e3edd98c65
Pavel Stetina <pavel.stetina@devcity.tech>
2022-12-05 20:05:48 +0100

    Advent of code 2022 day 05, second star.

--- 2022/05.php
+++ 2022/05.php
@@ -5,6 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $inputLoader = new \adventOfCode\lib\InputLoader(__FILE__, in_array('--sample', $argv));
 $stacks = [];
+$stacks9001 = null;
 
 foreach ($inputLoader as $line) {
 	if (preg_match('~\[[A-Z]\]~', $line)) {
@@ -21,20 +22,30 @@ foreach ($inputLoader as $line) {
 		foreach ($stacks as &$stack) {
 			$stack = array_reverse($stack);
 		}
+		unset($stack);
+		$stacks9001 = $stacks;
 		//\adventOfCode\lib\Dumper::dump($stacks);
 	}
 
 	if (preg_match('~^move (\d+) from (\d) to (\d)~', $line, $matches)) {
 		//\adventOfCode\lib\Dumper::dump($line);
+		$numberToMove = (int) $matches[1];
 		$from = (int) $matches[2];
 		$to = (int) $matches[3];
-		for ($i = (int) $matches[1]; $i > 0; --$i) {
+		for ($i = $numberToMove; $i > 0; --$i) {
 			$crate = array_pop($stacks[$from]);
 			array_push($stacks[$to], $crate);
 			//\adventOfCode\lib\Dumper::dump($crate);
 		}
+		$crates9001 = array_splice($stacks9001[$from], -$numberToMove);
+		array_push($stacks9001[$to], ...$crates9001);
+		//\adventOfCode\lib\Dumper::dump(...$crates9001);
 	}
 }
 
-$topCrates = array_map(fn($stack) => end($stack), $stacks);
+$readTopCrates = fn($stack) => end($stack);
+$topCrates = array_map($readTopCrates, $stacks);
 printf("Top crates: %s .\n", implode('', $topCrates));
+
+$topCrates = array_map($readTopCrates, $stacks9001);
+printf("Top crates 9001: %s .\n", implode('', $topCrates));
