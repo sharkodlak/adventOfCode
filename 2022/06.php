e0a345c401c7a0a84d14ece8db6606506b78ad99
Pavel Stetina <pavel.stetina@devcity.tech>
2022-12-06 18:24:29 +0100

    Advent of code 2022 day 06, second star.

--- 2022/06.php
+++ 2022/06.php
@@ -5,22 +5,41 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $inputLoader = new \adventOfCode\lib\InputLoader(__FILE__, in_array('--sample', $argv));
 $packetLength = 4;
+$messageMarkerLength = 14;
 
 foreach ($inputLoader as $line) {
+	$packetPos = null;
 	$lineLength = strlen($line);
 	for ($i = 0; $i < $lineLength; ++$i) {
-		$chunk = substr($line, $i, $packetLength);
-		//\adventOfCode\lib\Dumper::dump($chunk);
-		for ($aPos = 0; $aPos < $packetLength - 1; ++$aPos) {
-			for ($bPos = $aPos + 1; $bPos < $packetLength; ++$bPos) {
+		if (!isset($packetPos)) {
+			$chunk = substr($line, $i, $packetLength);
+			//\adventOfCode\lib\Dumper::dump($chunk);
+			for ($aPos = 0; $aPos < $packetLength - 1; ++$aPos) {
+				for ($bPos = $aPos + 1; $bPos < $packetLength; ++$bPos) {
+					if ($chunk[$aPos] === $chunk[$bPos]) {
+						//\adventOfCode\lib\Dumper::dump([$aPos => $chunk[$aPos], $bPos => $chunk[$bPos]]);
+						continue(3);
+					}
+				}
+			}
+			$packetPos = $i + $packetLength;
+		}
+
+		$chunk = substr($line, $i, $messageMarkerLength);
+		//\adventOfCode\lib\Dumper::dump([$i => $chunk]);
+		for ($aPos = 0; $aPos < $messageMarkerLength - 1; ++$aPos) {
+			for ($bPos = $aPos + 1; $bPos < $messageMarkerLength; ++$bPos) {
+				//\adventOfCode\lib\Dumper::dump([$aPos => $bPos]);
 				if ($chunk[$aPos] === $chunk[$bPos]) {
 					//\adventOfCode\lib\Dumper::dump([$aPos => $chunk[$aPos], $bPos => $chunk[$bPos]]);
 					continue(3);
 				}
 			}
 		}
+		$messagePos = $i + $messageMarkerLength;
 		break;
 	}
 	//\adventOfCode\lib\Dumper::dump(...$crates9001);
-	printf("Packet starts at: %s .\n", $i + $packetLength);
+	printf("Packet starts at: %s .\n", $packetPos);
+	printf("Message starts at: %s .\n", $messagePos);
 }
