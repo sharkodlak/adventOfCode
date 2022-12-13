6dfbf0eed26ea03e814fa3f34a046f5ba0e4c892
Pavel Stetina <pavel.stetina@devcity.tech>
2022-12-13 16:41:04 +0100

    Advent of code 2022 day 10, first star.

---
+++ 2022/10.php
@@ -0,0 +1,38 @@
+#!/usr/bin/env php
+<?php declare(strict_types=1);
+
+use Directory as GlobalDirectory;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$inputLoader = new \adventOfCode\lib\InputLoader(__FILE__, in_array('--sample', $argv));
+$clock = 0;
+$registerX = 1;
+$instructions = [
+	'noop' => ['cycles' => 1],
+	'addx' => ['cycles' => 2],
+];
+$watchedClocks = [20, 60, 100, 140, 180, 220];
+$sumOfSignalStrengths = 0;
+
+foreach ($inputLoader as $y => $line) {
+	$instruction = substr($line, 0, 4);
+	for ($c = 1; $c <= $instructions[$instruction]['cycles']; ++$c) {
+		$clock++;
+		if (in_array($clock, $watchedClocks)) {
+			//\adventOfCode\lib\Dumper::dump([$clock => $registerX]);
+			$sumOfSignalStrengths += $clock * $registerX;
+		}
+		if ($c === $instructions[$instruction]['cycles']) {
+			switch ($instruction) {
+				case 'addx':
+					$value = (int) substr($line, 5);
+					$registerX += $value;
+				break;
+			}
+		}
+	}
+	//\adventOfCode\lib\Dumper::dump([$instruction, $value, $registerX]);
+}
+
+printf("Sum of signal strengths: %d .\n", $sumOfSignalStrengths);
