6dce86bac9054248a6518c2c525b3d4f0b8088b3
Pavel Stetina <pavel.stetina@devcity.tech>
2022-12-14 21:39:49 +0100

    Advent of code 2022 day 11, second star.

--- 2022/11.php
+++ 2022/11.php
@@ -7,8 +7,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $inputLoader = new \adventOfCode\lib\InputLoader(__FILE__, in_array('--sample', $argv));
 $monkeys = [];
+$divisors = [];
 
 class Monkey {
+	public static int $compoundDivisor = 1;
 	private int $inspectedItems = 0;
 	private array $items;
 	private Closure $operation;
@@ -18,8 +20,7 @@ class Monkey {
 
 	public function __construct(
 		private int $order
-	) {
-	}
+	) {}
 
 	public function setOperation(Closure $formula) {
 		$this->operation = $formula;
@@ -28,6 +29,14 @@ class Monkey {
 	public function setTestDivision(int $divisor): self {
 		$this->divisor = $divisor;
 		return $this;
+	}
+
+	public function getPassIfTrue(): self {
+		return $this->passIfTrue;
+	}
+
+	public function getPassIfFalse(): self {
+		return $this->passIfFalse;
 	}
 
 	public function setPassIfTrue(self $monkey): self {
@@ -40,17 +49,21 @@ class Monkey {
 		return $this;
 	}
 
-	public function passItem(): bool {
+	public function passItem(bool $relief): bool {
 		$item = array_shift($this->items);
 		if ($item === null) {
 			return false;
 		}
 		$worryLevel = call_user_func($this->operation, $item);
-		$item = intval($worryLevel / 3);
+		$itemRetrospected = intval($relief ? $worryLevel / 3 : $worryLevel % self::$compoundDivisor);
 		$this->inspectedItems++;
-		$receivingMonkey = $item % $this->divisor === 0 ? $this->passIfTrue : $this->passIfFalse;
-		$receivingMonkey->add($item);
-		//var_dump(['monkey' => $this->order, 'worry' => $worryLevel, 'item' => $item, 'throw to' => $receivingMonkey->order]);
+		$receivingMonkey = $itemRetrospected % $this->divisor === 0 ? $this->passIfTrue : $this->passIfFalse;
+		$receivingMonkey->add($itemRetrospected);
+		if (is_float($worryLevel)) {
+			var_dump(['monkey' => $this->order, 'item' => $item, 'worry' => $worryLevel]);
+			exit;
+		}
+		//\adventOfCode\lib\Dumper::dump(['monkey' => $this->order, 'item' => $item, '*' => $worryLevel / $item, '+' => $worryLevel - $item, 'worry' => $worryLevel, 'itemRetrospected' => $itemRetrospected, 'throw to' => $receivingMonkey->order]);
 		return true;
 	}
 
@@ -94,6 +107,7 @@ foreach ($inputLoader as $line) {
 	} else if (substr($line, 0, 21) === '  Test: divisible by ') {
 		$divisor = (int) substr($line, 21);
 		$monkey->setTestDivision($divisor);
+		$divisors[] = $divisor;
 		//\adventOfCode\lib\Dumper::dump($formula);
 	} else if (substr($line, 0, 29) === '    If true: throw to monkey ') {
 		$i = (int) substr($line, 29);
@@ -109,19 +123,54 @@ foreach ($inputLoader as $line) {
 	//\adventOfCode\lib\Dumper::dump([$instruction, $value, $registerX]);
 }
 
+Monkey::$compoundDivisor = array_reduce($divisors, fn($carry, $item) => $carry * $item, 1);
 ksort($monkeys);
+$monkeysWithoutRelief = [];
+
+foreach ($monkeys as $i => $monkey) {
+	$monkeysWithoutRelief[$i] = clone $monkey;
+}
+
+foreach ($monkeysWithoutRelief as $monkey) {
+	$i = array_search($monkey->getPassIfTrue(), $monkeys);
+	$monkey->setPassIfTrue($monkeysWithoutRelief[$i]);
+	$i = array_search($monkey->getPassIfFalse(), $monkeys);
+	$monkey->setPassIfFalse($monkeysWithoutRelief[$i]);
+}
 
 for ($i = 0; $i < 20; ++$i) {
 	foreach ($monkeys as $monkey) {
-		while ($monkey->passItem()) {
+		while ($monkey->passItem(true)) {
 			// nothing
 		}
-	}	
+	}
 }
 
 $inspectedItems = [];
 
 foreach ($monkeys as $key => $monkey) {
+	$inspectedItems[$key] = $monkey->getInspectedItems();
+}
+
+rsort($inspectedItems);
+
+$monkeyMostInspectedItems = array_slice($inspectedItems, 0, 2);
+$product = array_reduce($monkeyMostInspectedItems, fn($carry, $inspectedItems) => $carry * $inspectedItems, 1);
+
+printf("Monkey bussiness: %d .\n", $product);
+
+
+for ($i = 0; $i < 10_000; ++$i) {
+	foreach ($monkeysWithoutRelief as $monkey) {
+		while ($monkey->passItem(false)) {
+			// nothing
+		}
+	}
+}
+
+$inspectedItems = [];
+
+foreach ($monkeysWithoutRelief as $key => $monkey) {
 	$inspectedItems[$key] = $monkey->getInspectedItems();
 }
 
